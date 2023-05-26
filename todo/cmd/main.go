95db634add10f1c7ef2b2194e309058e832a4078
hiroaqii <hiroaqii@gmail.com>
2023-05-27 02:30:53 +0900

    capturing input from STDIN

--- todo/cmd/main.go
+++ todo/cmd/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/hiroaqii/powerful-command-line-applications-in-go/todo"
 )
 
+func getTask(r io.Reader, arg ...string) (string, error) {
+	if len(arg) > 0 {
+		return strings.Join(arg, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
+}
+
 func main() {
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	task := flag.String("task", "", "Task to be included in the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
@@ -42,6 +64,17 @@ func main() {
 		}
 	case *task != "":
 		l.Add(*task)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *add:
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
