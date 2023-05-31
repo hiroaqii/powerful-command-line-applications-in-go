7aa5bdd988f51a0e5e153a515644d4dc932f5c08
hiroaqii <hiroaqii@gmail.com>
2023-05-31 23:39:30 +0900

    archiving files

--- walk/main.go
+++ walk/main.go
@@ -10,11 +10,12 @@ import (
 )
 
 type config struct {
-	ext  string
-	size int64
-	list bool
-	del  bool
-	wLog io.Writer
+	ext     string
+	size    int64
+	list    bool
+	del     bool
+	wLog    io.Writer
+	archive string
 }
 
 func run(root string, out io.Writer, cfg config) error {
@@ -33,6 +34,11 @@ func run(root string, out io.Writer, cfg config) error {
 				return listFile(path, out)
 			}
 
+			if cfg.archive != "" {
+				if err := archiveFile(cfg.archive, root, path); err != nil {
+					return err
+				}
+			}
 			if cfg.del {
 				return delFile(path, delLogger)
 			}
@@ -45,6 +51,7 @@ func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	logFile := flag.String("log", "", "Log deletes to this file	")
 	list := flag.Bool("list", false, "List files only")
+	archive := flag.String("archive", "", "Archive directory")
 	del := flag.Bool("del", false, "Delete files")
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
@@ -65,11 +72,12 @@ func main() {
 	}
 
 	c := config{
-		ext:  *ext,
-		size: *size,
-		list: *list,
-		del:  *del,
-		wLog: f,
+		ext:     *ext,
+		size:    *size,
+		list:    *list,
+		del:     *del,
+		wLog:    f,
+		archive: *archive,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
