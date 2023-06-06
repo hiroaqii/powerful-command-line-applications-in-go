ec3a6048f892ac62397054556e17ad87642c3c21
hiroaqii <hiroaqii@gmail.com>
2023-06-07 00:01:40 +0900

    improving the colStats tool to process files concurrently

--- colStats/main.go
+++ colStats/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func run(filenames []string, op string, column int, out io.Writer) error {
@@ -29,26 +30,51 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 
 	consolidate := make([]float64, 0)
 
+	resCh := make(chan []float64)
+	errCh := make(chan error)
+	doneCh := make(chan struct{})
+
+	wg := sync.WaitGroup{}
+
 	for _, fname := range filenames {
-		f, err := os.Open(fname)
-		if err != nil {
-			return fmt.Errorf("Cannot open file: %w", err)
-		}
+		wg.Add(1)
+		go func(fname string) {
+			defer wg.Done()
+			f, err := os.Open(fname)
+			if err != nil {
+				errCh <- fmt.Errorf("Cannot open file: %w", err)
+				return
+			}
 
-		data, err := csv2float(f, column)
-		if err != nil {
+			data, err := csv2float(f, column)
+			if err != nil {
+				errCh <- err
+			}
+
+			if err := f.Close(); err != nil {
+				errCh <- err
+			}
+
+			resCh <- data
+		}(fname)
+	}
+	go func() {
+		wg.Wait()
+		close(doneCh)
+	}()
+
+	for {
+		select {
+		case err := <-errCh:
+			return err
+		case data := <-resCh:
+			consolidate = append(consolidate, data...)
+		case <-doneCh:
+			_, err := fmt.Fprintln(out, opFunc(consolidate))
 			return err
 		}
-
-		if err := f.Close(); err != nil {
-			return err
-		}
-
-		consolidate = append(consolidate, data...)
 	}
 
-	_, err := fmt.Fprintln(out, opFunc(consolidate))
-	return err
 }
 
 func main() {
