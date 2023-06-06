2b685f20a61c9ebd5a603be685021cbb7073aadd
hiroaqii <hiroaqii@gmail.com>
2023-06-06 22:38:34 +0900

    reducing memory allocation

--- colStats/csv.go
+++ colStats/csv.go
@@ -23,14 +23,20 @@ func avg(data []float64) float64 {
 
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 	column--
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("Cannot read data from file: %w", err)
-	}
-
 	var data []float64
-	for i, row := range allData {
+
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read data from file: %w", err)
+		}
+
 		if i == 0 {
 			continue
 		}
@@ -40,12 +46,12 @@ func csv2float(r io.Reader, column int) ([]float64, error) {
 		}
 
 		v, err := strconv.ParseFloat(row[column], 64)
-
 		if err != nil {
 			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
 		}
 
 		data = append(data, v)
+
 	}
 
 	return data, nil
