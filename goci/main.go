0dceb6dda2d6578c7c44395e8237ded326d5d43a
hiroaqii <hiroaqii@gmail.com>
2023-06-10 18:20:40 +0900

    writing tests for goci

--- goci/main.go
+++ goci/main.go
@@ -13,12 +13,12 @@ func run(proj string, out io.Writer) error {
 		return fmt.Errorf("project directory is required: %w", ErrValidation)
 	}
 
-	args := []string{"build", ",", "errors"}
+	args := []string{"build", ".", "errors"}
 	cmd := exec.Command("go", args...)
 	cmd.Dir = proj
 
 	if err := cmd.Run(); err != nil {
-		return &stepErr{step: "go build", msg: "failed", cause: err}
+		return &stepErr{step: "go build", msg: "go build failed", cause: err}
 	}
 
 	_, err := fmt.Fprintln(out, "Go Build: SUCCESS")
