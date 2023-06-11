cfe3086a1633d28d87388bbc4a06c00113cac1d9
hiroaqii <hiroaqii@gmail.com>
2023-06-11 11:17:20 +0900

    testing commands with mock resources

--- goci/main_test.go
+++ goci/main_test.go
@@ -2,24 +2,158 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
+	"time"
 )
 
-func TestRun(t *testing.T) {
-	var testCase = []struct {
-		name   string
-		proj   string
-		out    string
-		expErr error
+func mockCmdContext(ctx context.Context, exe string, args ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess"}
+	cs = append(cs, exe)
+	cs = append(cs, args...)
+	cmd := exec.CommandContext(ctx, os.Args[0], cs...)
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
+}
+
+func mockCmdTimeout(cxt context.Context, exe string, args ...string) *exec.Cmd {
+	cmd := mockCmdContext(cxt, exe, args...)
+	cmd.Env = append(cmd.Env, "GO_HELPER_TIMEOUT=1")
+	return cmd
+}
+
+func TestHelperProcess(t *testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+
+	if os.Getenv("GO_HELPER_TIMEOUT") == "1" {
+		time.Sleep(15 * time.Second)
+	}
+
+	if os.Args[2] == "git" {
+		fmt.Fprintln(os.Stdout, "Everything up-to-date")
+		os.Exit(0)
+	}
+
+	os.Exit(0)
+}
+
+func setupGit(t *testing.T, proj string) func() {
+	t.Helper()
+
+	gitExec, err := exec.LookPath("git")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tempDir, err := ioutil.TempDir("", "gocitest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	projPath, err := filepath.Abs(proj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	remoteURI := fmt.Sprintf("file://%s", tempDir)
+
+	var gitCmdList = []struct {
+		args []string
+		dir  string
+		env  []string
 	}{
-		{name: "success", proj: "./testdata/tool/", out: "Go Build: SUCCESS\nGo Test: SUCCESS\nGofmt: SUCCESS\nGit Push: SUCCESS\n"},
-		{name: "fail", proj: "./testdata/toolErr", out: "", expErr: &stepErr{step: "go build"}},
-		{name: "failFormat", proj: "./testdata/toolFmtErr", out: "", expErr: &stepErr{step: "go fmt"}},
+		{[]string{"init", "--bare"}, tempDir, nil},
+		{[]string{"init"}, projPath, nil},
+		{[]string{"remote", "add", "origin", remoteURI}, projPath, nil},
+		{[]string{"add", "."}, projPath, nil},
+		{[]string{"commit", "-m", "test"}, projPath,
+			[]string{
+				"GIT_COMMITTER_NAME=test",
+				"GIT_COMMITTER_EMAIL=test@example.com",
+				"GIT_AUTHOR_NAME=test",
+				"GIT_AUTHOR_EMAIL=test@example.com",
+			}},
+	}
+
+	for _, g := range gitCmdList {
+		gitCmd := exec.Command(gitExec, g.args...)
+		gitCmd.Dir = g.dir
+
+		if g.env != nil {
+			gitCmd.Env = append(os.Environ(), g.env...)
+		}
+
+		if err := gitCmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return func() {
+		os.RemoveAll(tempDir)
+		os.RemoveAll(filepath.Join(projPath, ".git"))
+	}
+}
+func TestRun(t *testing.T) {
+
+	var testCase = []struct {
+		name     string
+		proj     string
+		out      string
+		expErr   error
+		setupGit bool
+		mockCmd  func(ctx context.Context, name string, arg ...string) *exec.Cmd
+	}{
+		{name: "successMock",
+			proj:     "./testdata/tool/",
+			expErr:   nil,
+			setupGit: false,
+			mockCmd:  mockCmdContext,
+			out:      "Go Build: SUCCESS\n" + "Go Test: SUCCESS\n" + "Gofmt: SUCCESS\n" + "Git Push: SUCCESS\n",
+		},
+		{name: "fail",
+			proj:     "./testdata/toolErr",
+			expErr:   &stepErr{step: "go build"},
+			setupGit: false,
+			mockCmd:  nil,
+			out:      ""},
+		{name: "failFormat",
+			proj:     "./testdata/toolFmtErr",
+			expErr:   &stepErr{step: "go fmt"},
+			setupGit: false,
+			mockCmd:  nil,
+			out:      ""},
+		{name: "failTimeout",
+			proj:     "./testdata/tool",
+			expErr:   context.DeadlineExceeded,
+			setupGit: false,
+			mockCmd:  mockCmdTimeout,
+			out:      ""},
 	}
 
 	for _, tc := range testCase {
 		t.Run(tc.name, func(t *testing.T) {
+			if tc.setupGit {
+				_, err := exec.LookPath("git")
+				if err != nil {
+					t.Skip("Git not installed. Skipping test.")
+				}
+
+				cleanup := setupGit(t, tc.proj)
+				defer cleanup()
+			}
+
+			if tc.mockCmd != nil {
+				command = tc.mockCmd
+			}
+
 			var out bytes.Buffer
 			err := run(tc.proj, &out)
 
